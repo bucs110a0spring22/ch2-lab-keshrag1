9c320ead0de0221d48eb998f8e5d8a24bd0fb2dd
Kia S S <98551204+keshrag1@users.noreply.github.com>
2022-02-07 20:34:39 +0000

    I added the programs to calculate cost per class and found the type of variables

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
-
-
+classes_per_week = 3
+cost_per_class = (cost_per_week / classes_per_week)
+print("cost_per_class:", cost_per_class, "dollars per class")
+print(weeks, type(weeks))
+print(classes, type(classes))
+print(tuition, type(tuition))
+print(cost_per_week, type(cost_per_week))
+print(classes_per_week, type(classes_per_week))
+print(cost_per_class, type(cost_per_class))
 #Part B
